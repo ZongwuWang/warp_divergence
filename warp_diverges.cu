d56a4d5cf9f0599ab56bb4670277e3100abc35a7
root <root@jiangli1-0.jiangli1.podvm.svc.cluster.local>
2024-08-05 22:50:19 +0800

    tensor and cuda instruction interleaved

--- warp_diverges.cu
+++ warp_diverges.cu
@@ -35,7 +35,7 @@ __global__ void warp_divergent_kernel_hete(int *a, half *b, half *c, float *d, i
             a[index] += *val;
 			// __nanosleep(10000);
         }
-    } else {
+    } 
 		for (int i = 0; i < 10000; ++i) {
 			// 声明用于 MMA 操作的片段
 			wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> b_frag;
@@ -56,7 +56,46 @@ __global__ void warp_divergent_kernel_hete(int *a, half *b, half *c, float *d, i
 			// 将结果写回到全局内存
 			wmma::store_matrix_sync(d, acc_frag, WMMA_K, wmma::mem_row_major);
 		}		
-    }
+    
+}
+
+__global__ void warp_divergent_kernel_hete2(int *a, half *b, half *c, float *d, int *val) {
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    // if (index % 2 == 0) {
+    //     for (int i = 0; i < 20000; ++i) {
+    //         a[index] += *val;
+	// 		// __nanosleep(10000);
+    //     }
+    // } 
+		for (int i = 0; i < 10000; ++i) {
+			// 声明用于 MMA 操作的片段
+			wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> b_frag;
+			wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major> c_frag;
+			wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> acc_frag;
+			// wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> d_frag;
+
+			// 初始化累加器片段
+			wmma::fill_fragment(acc_frag, 0.0f);
+			
+			// 加载矩阵 A 和 B 到片段中
+			wmma::load_matrix_sync(b_frag, b, WMMA_K);
+			wmma::load_matrix_sync(c_frag, c, WMMA_K);
+
+			// 执行矩阵乘法和累加操作
+			wmma::mma_sync(acc_frag, b_frag, c_frag, acc_frag);
+
+			// 利用tensor core计算期间并行进行cuda core的操作
+			a[index] += *val;
+			if (index < 16) {
+				for (int j = 0; j < 4; ++j) {
+					a[index * 2] += a[index * 2 + 1];
+				}
+			}
+
+			// 将结果写回到全局内存
+			wmma::store_matrix_sync(d, acc_frag, WMMA_K, wmma::mem_row_major);
+		}		
+    
 }
 
 // Kernel with warp divergence
@@ -112,7 +151,7 @@ int main(void) {
 
     // Launch kernels and measure execution time
     cudaEvent_t start, stop;
-    float time1, time2, time3, time4;
+    float time1, time2, time3, time4, time5;
 
 	for (int i = 0; i < 50; i++) {
 
@@ -156,6 +195,16 @@ int main(void) {
 		cudaEventSynchronize(stop);
 		cudaEventElapsedTime(&time4, start, stop);
 		printf("Time for the cuda core and tensor core (heterogeneous) kernel with warp divergence: %f ms\n", time4);
+
+		// Tensor core kernel
+		cudaEventCreate(&start);
+		cudaEventCreate(&stop);
+		cudaEventRecord(start, 0);
+		warp_divergent_kernel_hete2<<<1, THREADS_PER_BLOCK>>>(a, b, c, d, val);
+		cudaEventRecord(stop, 0);
+		cudaEventSynchronize(stop);
+		cudaEventElapsedTime(&time5, start, stop);
+		printf("Time for the cuda core and tensor core (heterogeneous) kernel2 with warp divergence: %f ms\n", time5);
 
 		printf("============================================\n");
 		// printf("Without warp divergence speedup compared to with warp divergence: %f\n", time1 / (time2 + time3));
